342550ea05f83421b273cda4625a46ce95447418
PanS4ge <69396555+PanS4ge@users.noreply.github.com>
2024-02-16 12:17:01 +0100

    Create Program.cs

---
+++ C# Files/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MelonLoader;
+using SampleMod;
+
+// Always remember about this file
+
+// You can change MelonInfo
+
+//    typeof(Name of the class)
+//                    \/      Mod name shown
+//                    \/              \/     Mod Version
+//                    \/              \/            \/  You <3 \/
+[assembly: MelonInfo(typeof(Krnl), "Your Mod", "Version", "You")]
+
+// This you leave alone, It's already prepared to work.
+[assembly: MelonGame("Dev Kacper", "DunHero")]
