220c609444e2a47e25a7882856db7e0779f1ab4d
PanS4ge <69396555+PanS4ge@users.noreply.github.com>
2024-02-16 12:17:22 +0100

    Create WhichScene.cs

---
+++ C# Files/WhichScene.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleMod
+{
+    // That's all
+    public enum WhichScene
+    {
+        MAIN_MENU,
+        LOADING_SCREEN,
+        MAIN_GAMEPLAY,
+        SOME_OTHER
+    }
+}
